fd375189baf164a40d99d57a0542bf52e65744f8
baderAmmoun <baderfull@hotmail.com>
2020-11-06 16:54:46 +0100

    delete the refrence of applicationInit refrence from the main method

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ApplicationInit mainWindow=new ApplicationInit(primaryStage, BattleViewClassic.getInstance());
+        new ApplicationInit(primaryStage, BattleViewClassic.getInstance());
 
     }
 }
